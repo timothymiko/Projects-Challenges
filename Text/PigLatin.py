616d55be2d99fb40ed5d6f2ac9864c0497bfca7d
Timothy Miko <timothymiko@gmail.com>
2013-08-15 14:39:20 -0400

    Update PigLatin.py
    Added support for sentences

--- Text/PigLatin.py
+++ Text/PigLatin.py
@@ -1,24 +1,29 @@
-str = input('Enter your word: ')
+input = input('Enter your word: ')
+
+words = input.split()
+nwords = []
 new = ''
 
-count = 0
-firstVow = 0
-num = 0
-
-while count < len(str):
-	if str[count] in 'aeiouy':	
-		firstVow = count
-		while firstVow < len(str):
-			new += str[firstVow]
-			firstVow += 1
-		while num < count:
-			new += str[num]
-			num += 1
-		if str[0] in 'aeiou':
-			new += 'way'
-		else:
-			new += 'ay'
-		count = len(str)
-	count += 1
-
-print(new)
+for str in words:
+	count = 0
+	firstVow = 0
+	num = 0
+	while count < len(str):
+		if str[count] in 'aeiouy':
+			new = ''	
+			firstVow = count
+			while firstVow < len(str):
+				new += str[firstVow]
+				firstVow += 1
+			while num < count:
+				new += str[num]
+				num += 1
+			if str[0] in 'aeiou':
+				new += 'way'
+			else:
+				new += 'ay'
+			count = len(str)
+			nwords.append(new)
+		count += 1
+		
+print(' '.join(nwords))
